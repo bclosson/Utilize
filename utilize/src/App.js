049bf511f9b04bd74f5e2afb8fb27347e449ccd9
benjaminclosson1 <benjaminclosson1@gmail.com>
2020-11-30 12:48:51 -0800

    commit made navbar routes functional, create recipeCard file

--- utilize/src/App.js
+++ utilize/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Navbar from './Components/Navbar';
 import routes from './config/routes';
-
 
 function App() {
   return (
     <div className="App">
+     <Navbar />
+     { routes }
      <h1>Utilize!</h1>
     </div>
   );
---
+++ utilize/src/Components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar(props) {
+  return(
+    <nav>
+      <div className="nav-wrapper">
+        <Link href="#" className="brand-logo">UTILIZE!</Link>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/recipes">Recipe Library</Link></li>
+          <li><Link to="/recipes/new">Find Recipes</Link></li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar;
