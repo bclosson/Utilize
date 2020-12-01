158007c4ecf42bd0042c484ddc8cfc3b61ee0e7b
benjaminclosson1 <benjaminclosson1@gmail.com>
2020-11-30 21:14:43 -0800

    commit added routes to register and login pages

--- client/src/config/routes.js
+++ client/src/config/routes.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from '../pages/Home';
+import Register from '../Components/auth/Register';
+import Login from '../Components/auth/Login';
 import RecipeList from '../pages/RecipeList';
 import NewRecipe from '../pages/NewRecipe';
 import RecipeShow from '../pages/RecipeShow';
@@ -9,6 +11,8 @@ import RecipeShow from '../pages/RecipeShow';
 export default (
   <Switch>
     <Route exact path='/' component={ Home } />
+    <Route path='/register' component={ Register } />
+    <Route path='/login' component={ Login } />
     <Route path='/recipes/new' component={ NewRecipe } />
     <Route path='/recipes/:id' component={ RecipeShow } />
     <Route path='/recipes' component={ RecipeList } />
