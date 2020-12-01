badad87cd0f3b786ff7a2c10801c0943561ab7b1
benjaminclosson1 <benjaminclosson1@gmail.com>
2020-11-30 16:01:44 -0800

    commit set up passport middleware, config, and route in server.js

--- utilize-back-end/server.js
+++ utilize-back-end/server.js
@@ -5,10 +5,17 @@ const routes = require('./routes');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 4001;
 const app = express();
+const passport = require('passport');
+const users = require('./routes/api/users');
 
 const corsOptions = {
   origin: 'http://localhost:3000'
 }
+// PASSPORT MIDDLEWARE
+app.use(passport.initialize());
+
+// PASSPORT CONFIG
+require('./config/passport')(passport);
 
 // MIDDLEWARE - BODY PARSER
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,7 +26,7 @@ app.use(cors(corsOptions));
 
 // MIDDLEWARE - API ROUTES
 app.use('/api/v1/recipes', routes.recipes);
-
+app.use('/api/users', users)
 
 // CONNECTION
 app.listen(port, () => console.log(`Server is running on port ${port}`));
