984fa028c2b210810e56367f1d8959d8b0a8703c
benjaminclosson1 <benjaminclosson1@gmail.com>
2020-12-02 08:55:12 -0800

    commit added form to new recipe page

--- client/src/pages/NewRecipe.js
+++ client/src/pages/NewRecipe.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import RecipeModel from '../models/recipe';
 import RecipeCard from '../Components/RecipeCard';
 
 
@@ -14,8 +13,51 @@ class NewRecipe extends React.Component {
 
   render() {
     return (
-      <div>
-        <RecipeCard />
+      <div className="container">
+        <div className="form">
+          <div className="row">
+            <form className="col s12">
+              <div className="row">
+                <div className="input-field col s6">
+                  <input placeholder="Placeholder" id="produce" type="text" className="validate" />
+                  <label for="produce">Produce</label>
+                </div>
+                <div className="input-field col s6">
+                  <input id="meat" type="text" className="validate" />
+                  <label for="meat">Meat</label>
+                </div>
+              </div>
+              <div className="row">
+                <div className="input-field col s6">
+                  <input id="seafood" type="text" className="validate" />
+                  <label for="seafood">Seafood</label>
+                </div>
+              </div>
+              <div className="row">
+                <div className="input-field col s6">
+                  <input id="dairy" type="text" className="validate" />
+                  <label for="dairy">Dairy</label>
+                </div>
+              </div>
+              <div className="row">
+                <div className="input-field col s6">
+                  <input id="spices" type="text" className="validate" />
+                  <label for="spices">Spices</label>
+                </div>
+              </div>
+              <div className="row">
+                <div className="input-field col s6">      
+                    <input id="email_inline" type="email" className="validate" />
+                    <label for="email_inline">Email</label>
+                    <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="card">
+          <RecipeCard />
+        </div>
       </div>
     )
   };
