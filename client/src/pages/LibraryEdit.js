2f8c9ac1ae83abe2baea35b4a913d49608a41043
benjaminclosson1 <benjaminclosson1@gmail.com>
2020-12-10 09:42:38 -0800

    revised edit form

--- client/src/pages/LibraryEdit.js
+++ client/src/pages/LibraryEdit.js
@@ -38,39 +38,23 @@ class LibraryEdit extends React.Component {
         <div className="row">
           <div className="input-field col s6">
             <input placeholder="Placeholder" id="first_name" type="text" className="validate" />
-            <label for="first_name">First Name</label>
+            <label for="first_name">Recipe Name</label>
           </div>
           <div className="input-field col s6">
             <input id="last_name" type="text" className="validate" />
-            <label for="last_name">Last Name</label>
+            <label for="last_name">Recipe Image</label>
           </div>
         </div>
         <div className="row">
           <div className="input-field col s12">
             <input disabled value="I am not editable" id="disabled" type="text" className="validate" />
-            <label for="disabled">Disabled</label>
+            <label for="disabled">Ingredients</label>
           </div>
         </div>
         <div className="row">
           <div className="input-field col s12">
             <input id="password" type="password" className="validate" />
-            <label for="password">Password</label>
-          </div>
-        </div>
-        <div className="row">
-          <div className="input-field col s12">
-            <input id="email" type="email" className="validate" />
-            <label for="email">Email</label>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col s12">
-            This is an inline input field:
-            <div className="input-field inline">
-              <input id="email_inline" type="email" className="validate" />
-              <label for="email_inline">Email</label>
-              <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
-            </div>
+            <label for="password">Servings: </label>
           </div>
         </div>
         <div className="row">
@@ -78,15 +62,15 @@ class LibraryEdit extends React.Component {
             <div className="row">
               <div className="input-field col s12">
                 <textarea id="textarea1" className="materialize-textarea"></textarea>
-                <label for="textarea1">Textarea</label>
+                <label for="textarea1">Comments: </label>
               </div>
             </div>
           </form>
         </div>
       </form>
     </div>
-  )
+    )
+  }
 }
-
 
 export default LibraryEdit;
