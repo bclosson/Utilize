901a1c33742c4e0cc97f31619069f48318cc9f18
benjaminclosson1 <benjaminclosson1@gmail.com>
2020-12-14 11:49:16 -0800

    commit fixed footer overlap

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -2,16 +2,17 @@
 function Footer() {
 return(
     <footer className="page-footer">
-      <div className="container">
+      <div className="container" id="footer-container">
         <div className="row">
           <div id="edamam-badge" data-color="white"></div>
         </div>
-      </div>
-      <div className="footer-copyright">
-        <div className="container">
+        <div className="container" id="copywrite">
         © 2020 Copyright UTILIZE!
         </div>
       </div>
+      {/* <div className="footer-copyright">
+       
+      </div> */}
     </footer>
   );
 };
