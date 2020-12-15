e6af45ddbd71a58bc90f51d8f48d3183548a0780
benjaminclosson1 <benjaminclosson1@gmail.com>
2020-12-14 16:00:27 -0800

    commit added box shadow and rounded edges to libcard

--- client/src/Components/LibraryCards.js
+++ client/src/Components/LibraryCards.js
@@ -7,9 +7,9 @@ const LibraryCards = (props) => {
   return (
     <div className="row">
     <div className="col s6 m7">
-      <div className="card">
+      <div className="card" id="library-recipe">
         <div className="card-image">
-          <img src={props.recipe.image} alt="Recipe"/>
+          <img src={props.recipe.image} id="library-recipe-image" alt="Recipe"/>
           <span className="card-title">{ props.recipe.name }</span>
         </div>
         <div className="card-content">
@@ -17,7 +17,7 @@ const LibraryCards = (props) => {
           <p>Calories Per Serving: { Math.floor(props.recipe.calories / props.recipe.servings) }</p>
           
         </div>
-        <div className="card-action">
+        <div className="card-action" id="card-action">
           <Link to={`/recipes/library/${props.recipe._id}`}>
             Click For Full Recipe
           </Link>
