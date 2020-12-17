97b3ddd4faf9a7ceb8c3b1d3e07ed80d64ccc040
benjaminclosson1 <benjaminclosson1@gmail.com>
2020-12-16 18:11:03 -0800

    commit adjusted server.js routes for production

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 4001;
 const app = express();
 const passport = require('passport');
 const users = require('./routes/api/users');
+const path = require('path');
 
 const corsOptions = {
   origin: 'http://localhost:3000'
@@ -27,6 +28,16 @@ app.use(cors(corsOptions));
 // MIDDLEWARE - API ROUTES
 app.use('/api/v1/recipes', routes.recipes);
 app.use('/api/users', users)
+
+// SERVE STATIC ASSETS IF IN PRODUCTION
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // CONNECTION
 app.listen(port, () => console.log(`Server is running on port ${port}`));
