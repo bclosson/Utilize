c50a42bf310ff3f5a97b61763063931ae1d14c8c
benjaminclosson1 <benjaminclosson1@gmail.com>
2020-12-18 17:12:41 -0800

    commit changed server.js file

--- server.js
+++ server.js
@@ -33,10 +33,14 @@ app.use('/api/users', users)
 // SERVE STATIC ASSETS IF IN PRODUCTION
 if (process.env.NODE_ENV === 'production') {
   // set static folder
-  app.use(express.static('client/build'));
+  // app.use(express.static('client/build'));
 
-  app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  // app.get('/*', (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  // });
+  app.use(express.static(__dirname));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
   });
 }
 
