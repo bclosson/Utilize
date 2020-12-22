7464d765676cfce48237bc8259195d820d4324d3
benjaminclosson1 <benjaminclosson1@gmail.com>
2020-12-21 18:54:29 -0800

    brought back original serving of static assests

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const PORT = process.env.PORT || 4001;
 const app = express();
 const passport = require('passport');
 const users = require('./routes/api/users');
+const recipes = require('./routes/recipes');
 const path = require('path');
-const recipes = require("./routes/recipes");
 
 const corsOptions = {
   // origin: 'http://localhost:3000'
@@ -32,18 +32,18 @@ app.use('/api/v1/recipes', recipes);
 app.use('/api/users', users)
 
 // Testing new approach HERE
-app.use(express.static(path.join(__dirname, "client", "build")));
+// app.use(express.static(path.join(__dirname, "client", "build")));
 
 // SERVE STATIC ASSETS IF IN PRODUCTION
 if (process.env.NODE_ENV === 'production') {
   // set static folder
-  // app.use(express.static('client/build'));
+  app.use(express.static('client/build'));
 
   // app.get('/*', (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  //   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   // });
   app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
