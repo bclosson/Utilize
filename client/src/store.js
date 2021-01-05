01c1bbde52bc315de21f8096f5c5215635546a1e
benjaminclosson1 <benjaminclosson1@gmail.com>
2021-01-05 14:17:05 -0800

    commit reconfigured store.js with composeWithDevTools

--- client/src/store.js
+++ client/src/store.js
@@ -1,18 +1,36 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const initialState = {};
+export default function configureStore(preloadedState) {
+  const middleware = [thunk];
+  const middlewareEnhancer = applyMiddleware(...middleware)
 
-const middleware = [thunk];
-const store = createStore(
-  rootReducer,
-  initialState,
-    compose(
-      applyMiddleware(...middleware),
-      window.__REDUX_DEVTOOLS_EXTENSION__&&
-      window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+  const enhancers = [middlewareEnhancer]
+  const composedEnhancers = composeWithDevTools(...enhancers)
+  const store = createStore(rootReducer, preloadedState, composedEnhancers)
 
-export default store;
+  return store
+}
+
+
+
+
+
+
+
+// const initialState = {};
+
+// const middleware = [thunk];
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//     compose(
+//       applyMiddleware(...middleware),
+//       window.__REDUX_DEVTOOLS_EXTENSION__&&
+//       window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
+
+// export default store;
