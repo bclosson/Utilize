c8ea4e9f8303f0d089e3b19509a4ad9439a4ed1f
benjaminclosson1 <benjaminclosson1@gmail.com>
2021-01-11 17:27:23 -0800

    commit revised store.js to remove composeEnhancers

--- client/src/store.js
+++ client/src/store.js
@@ -1,18 +1,16 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 
 const initialState = {};
-const composeEnhancers = (typeof window !== 'undefined' 
-  && window.__REDUX_DEVTOOLS_EXTENSIONS_COMPOSE__) || compose;
 
 const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  composeEnhancers(
+  composeWithDevTools(
   applyMiddleware(...middleware)
       // window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__&&
       // window.__REDUX_DEVTOOLS_EXTENSION__()
