0a0692807849b9f91a49545a74cc5e162e979b6f
benjaminclosson1 <benjaminclosson1@gmail.com>
2021-01-11 17:06:48 -0800

    modified composeDevTools in store.js

--- client/src/store.js
+++ client/src/store.js
@@ -1,11 +1,11 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 
 const initialState = {};
-const composeEnhancers = (typeof window !== 'undefined' 
+const composeEnhancers = composeWithDevTools(typeof window !== 'undefined' 
   && window.__REDUX_DEVTOOLS_EXTENSIONS_COMPOSE__) || compose;
 
 const middleware = [thunk];
